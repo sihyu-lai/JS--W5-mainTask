592f5ba2f056b543e2c45581ffe9551f416c4c79
justrecommand <77605403+justrecommand@users.noreply.github.com>
2021-06-14 23:12:05 +0800

    0614

---
+++ JS-W5-LvV3-0613/js/all.js
@@ -0,0 +1,107 @@
+// 地區用 change 監聽-->通常用於表單
+// 上方新增的地區跟下方篩選的地區都寫死選項（依照目前提供的 JSON data area 欄位）
+// 地區的篩選下拉需要加上『全部地區』option
+// 不需要有「清除資料」的按鈕
+// 預設資料為 3 筆（內容需依照目前提供的 JSON data）
+// 篩選後會顯示『搜尋資料為 ? 筆』
+// 描述欄位使用 textarea  這
+// 星級區間是 1-10 分
+// 金額、組數、星級的 type 為 Number
+let data = [
+    {
+      "id": 0,
+      "name": "肥宅心碎賞櫻3日",
+      "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
+      "area": "高雄",
+      "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
+      "group": 87,
+      "price": 1400,
+      "rate": 10
+    },
+    {
+      "id": 1,
+      "name": "貓空纜車雙程票",
+      "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+      "area": "台北",
+      "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
+      "group": 99,
+      "price": 240,
+      "rate": 2
+    },
+    {
+      "id": 2,
+      "name": "台中谷關溫泉會1日",
+      "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+      "area": "台中",
+      "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
+      "group": 20,
+      "price": 1765,
+      "rate": 7
+    }
+  ];
+
+ 
+const formSelect=document.querySelector('.formselect');
+const selectNum=document.querySelector('.selectnum'); 
+const  cardArea=document.querySelector('.cardarea');
+let str=``;
+//更換資料-初始化資料/*
+function init(){
+    data.forEach(function(item){
+    let content= `<div class="card shadow  mb-5 bg-body rounded  position-relative cardset">
+    <img src="${item.imgUrl}" class="card-img-top" alt="...">
+    <span class="position-absolute top-0    badge   bg-secondary fs-3 py-4 px-4 ">${item.area} <span class="visually-hidden">unread messages</span></span> 
+    <span class="position-absolute  top-50    badge   bg-primary fs-4 py-2 px-2 ">${item.rate}<span class="visually-hidden">unread messages</span></span> 
+    <div class="card-body  ">
+     
+      <h5 class="card-title text-primary border-bottom border-primary border-2 pb-3 fs-3">${item.name}</h5>
+      <p class="card-text text-info fs-5 h-50">${item.description}</p>
+      <div class="d-flex justify-content-between">
+      <a href="#" class="text-primary fw-bolder text-decoration-none align-self-center"><span class="iconify" data-icon="akar-icons:circle-plus-fill" data-inline="false" style="color: #00807E;"></span>剩下最後<span>${item.group}</span>組</a>
+      <a href="#" class="text-primary fw-bolder text-decoration-none">TWD<span class="fs-3">${item.price}</span></a>
+    </div>
+    </div>
+    </div>`;
+    str+=content;
+    
+    });
+    
+     cardArea.innerHTML=str;
+    }
+    init();*/
+    
+
+ 
+// 篩選
+ formSelect.addEventListener('change',function(e){
+    console.log(e.target.value);
+    if(e.target.value==undefined){
+        alert('您選取無效');
+        return;
+    }
+     data.forEach(function(item,index){
+        if(e.target.value==item.area){
+            str= `<div class="card shadow  mb-5 bg-body rounded  position-relative cardset">
+            <img src="${item.imgUrl}" class="card-img-top" alt="...">
+            <span class="position-absolute top-0    badge   bg-secondary fs-3 py-4 px-4 ">${item.area} <span class="visually-hidden">unread messages</span></span> 
+            <span class="position-absolute  top-50    badge   bg-primary fs-4 py-2 px-2 ">${item.rate}<span class="visually-hidden">unread messages</span></span> 
+            <div class="card-body  ">
+             
+              <h5 class="card-title text-primary border-bottom border-primary border-2 pb-3 fs-3">${item.name}</h5>
+              <p class="card-text text-info fs-5 h-50">${item.description}</p>
+              <div class="d-flex justify-content-between">
+              <a href="#" class="text-primary fw-bolder text-decoration-none align-self-center"><span class="iconify" data-icon="akar-icons:circle-plus-fill" data-inline="false" style="color: #00807E;"></span>剩下最後<span>${item.group}</span>組</a>
+              <a href="#" class="text-primary fw-bolder text-decoration-none">TWD<span class="fs-3">${item.price}</span></a>
+            </div>
+            </div>
+            </div> `;
+          }else if(e.target.value=="全部地區"){
+             
+          }
+
+    })
+
+    
+    cardArea.innerHTML=str;
+
+  });
