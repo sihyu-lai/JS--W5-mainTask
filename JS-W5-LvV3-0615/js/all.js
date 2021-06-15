2193e1777ff3210ac3f0109a6f5ffbb4463115a8
justrecommand <77605403+justrecommand@users.noreply.github.com>
2021-06-15 20:49:20 +0800

    2021-06-15修正
    目前有init（）的問題

--- JS-W5-LvV3-0615/js/all.js
+++ JS-W5-LvV3-0615/js/all.js
@@ -40,14 +40,17 @@ let data = [
     }
   ];
 
- 
-const formSelect=document.querySelector('.formselect');
-const selectNum=document.querySelector('.selectnum'); 
-const  cardArea=document.querySelector('.cardarea');
-let str=``;
-//更換資料-初始化資料/*
-function init(){
+//宣告變數
+const formSelect=document.querySelector('.formselect'); // 切換的下拉表單
+const searchNum=document.querySelector('.searchNum');  // 搜尋資料筆數
+const  cardArea=document.querySelector('.cardarea'); // 資料畫面
+
+//更換資料-初始化資料
+function init( ){
+    let str=``;
+    let searchCount=0; //預設搜尋幾筆
     data.forEach(function(item){
+        searchCount++;
     let content= `<div class="card shadow  mb-5 bg-body rounded  position-relative cardset">
     <img src="${item.imgUrl}" class="card-img-top" alt="...">
     <span class="position-absolute top-0    badge   bg-secondary fs-3 py-4 px-4 ">${item.area} <span class="visually-hidden">unread messages</span></span> 
@@ -63,45 +66,58 @@ function init(){
     </div>
     </div>`;
     str+=content;
-    
+    searchNum.textContent=searchCount;
+     
     });
-    
+     
      cardArea.innerHTML=str;
     }
-    init();*/
+init();
     
 
  
 // 篩選
  formSelect.addEventListener('change',function(e){
-    console.log(e.target.value);
-    if(e.target.value==undefined){
-        alert('您選取無效');
-        return;
-    }
-     data.forEach(function(item,index){
-        if(e.target.value==item.area){
-            str= `<div class="card shadow  mb-5 bg-body rounded  position-relative cardset">
-            <img src="${item.imgUrl}" class="card-img-top" alt="...">
-            <span class="position-absolute top-0    badge   bg-secondary fs-3 py-4 px-4 ">${item.area} <span class="visually-hidden">unread messages</span></span> 
-            <span class="position-absolute  top-50    badge   bg-primary fs-4 py-2 px-2 ">${item.rate}<span class="visually-hidden">unread messages</span></span> 
-            <div class="card-body  ">
-             
-              <h5 class="card-title text-primary border-bottom border-primary border-2 pb-3 fs-3">${item.name}</h5>
-              <p class="card-text text-info fs-5 h-50">${item.description}</p>
-              <div class="d-flex justify-content-between">
-              <a href="#" class="text-primary fw-bolder text-decoration-none align-self-center"><span class="iconify" data-icon="akar-icons:circle-plus-fill" data-inline="false" style="color: #00807E;"></span>剩下最後<span>${item.group}</span>組</a>
-              <a href="#" class="text-primary fw-bolder text-decoration-none">TWD<span class="fs-3">${item.price}</span></a>
-            </div>
-            </div>
-            </div> `;
-          }else if(e.target.value=="全部地區"){
-             
-          }
+    // console.log(e.target.value);
+    data.forEach(function(item,index){
+      if(e.target.value==data[index].area){
+        // console.log(e.target.value);
+        // console.log(data[index].area);
+        // 渲染畫面＋搜尋的筆數
+        let searchCount=0;
+        searchCount++;
+         let str=``;
+         let content= `<div class="card shadow  mb-5 bg-body rounded  position-relative cardset">
+    <img src="${item.imgUrl}" class="card-img-top" alt="...">
+    <span class="position-absolute top-0    badge   bg-secondary fs-3 py-4 px-4 ">${item.area} <span class="visually-hidden">unread messages</span></span> 
+    <span class="position-absolute  top-50    badge   bg-primary fs-4 py-2 px-2 ">${item.rate}<span class="visually-hidden">unread messages</span></span> 
+    <div class="card-body  ">
+     
+      <h5 class="card-title text-primary border-bottom border-primary border-2 pb-3 fs-3">${item.name}</h5>
+      <p class="card-text text-info fs-5 h-50">${item.description}</p>
+      <div class="d-flex justify-content-between">
+      <a href="#" class="text-primary fw-bolder text-decoration-none align-self-center"><span class="iconify" data-icon="akar-icons:circle-plus-fill" data-inline="false" style="color: #00807E;"></span>剩下最後<span>${item.group}</span>組</a>
+      <a href="#" class="text-primary fw-bolder text-decoration-none">TWD<span class="fs-3">${item.price}</span></a>
+    </div>
+    </div>
+    </div>`;
+        str+=content;
+        cardArea.innerHTML=str;
+        searchNum.textContent=searchCount;
+      }else if(e.target.value=="全部地區"){
+        return init();
+      }
+     
+    });
 
-    })
+    //選擇全部地區，出現init();
+    //取不到值
+   
 
     
-    cardArea.innerHTML=str;
+    //目的：當我選擇台中的時候，只會出現台中的資料;搜尋筆數隨之增加。
+    // if(e.target.value==data[item].area)
+
+   // 選擇全部地區，就出現全部資料
 
   });
